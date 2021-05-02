fb127407c2ddc00b62dff0282149d52980e7b0c0
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2021-05-02 16:01:08 +0900

    例外処理の勉強

--- books_admin/app/controllers/application_controller.rb
+++ books_admin/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from LoginFailed, with: :login_failed # LoginFailed例外が発生したらlogin_failedメソッドを呼び出してrescue
+
+  def login_failed
+    render template: "shared/login_failed", status: 401 # 401コードとともにlogin_failedテンプレートを返す
+  end
 end
